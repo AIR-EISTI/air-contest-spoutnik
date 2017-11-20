7da3bd02a98b1204c615f637e027a76208e4f695
Tjiho Lun <tom.darboux@eisti.fr>
2017-11-20 17:31:06 +0100

    [NAV BAR]:add hamburger button

--- users/src/components/NavBar.vue
+++ users/src/components/NavBar.vue
@@ -1,11 +1,14 @@
 <template>
-    <nav id="navbar">
+    <nav id="navbar" v-bind:class="{ noVisible: noVisible }">
+        <img class="hamburger" src="/static/icons/IcoMoon/SVG/190-menu.svg" @click="displayMenu"/>
+        <div class="content-menu">
         <router-link :to="{name: 'Home'}"><h1>Air-contest</h1></router-link>
         <ul>
             <a href="#"><li>Listes des exercices</li></a>
             <a href="#"><li>Les tournois</li></a>
             <router-link :to="{name: 'Login'}"><li>Login</li></router-link>
         </ul>
+        </div>
     </nav>
 </template>
 
@@ -14,13 +17,19 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      noVisible: false
     }
-  }
+  },
+  methods: {
+    displayMenu () {
+      this.noVisible = !this.noVisible
+    }
+  },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+  
 </style>
