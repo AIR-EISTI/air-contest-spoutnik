cfe18116f1685e32bc9d819853a08e8d9caaa00c
Tjiho Lun <tom.darboux@eisti.fr>
2017-11-23 15:28:16 +0100

    [CSS]:better navbar + white text for tournoi in homepage

--- users/src/components/NavBar.vue
+++ users/src/components/NavBar.vue
@@ -31,5 +31,145 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  /*-- -- nav bar -- --*/
+
+#navbar
+{    
+    display: flex;
+    align-items: center;
+    position: fixed;
+    top:0;
+    left: 0;
+    width: 100vw;
+    z-index: 5;
+    background-color: white;
+    flex-wrap: 0;
+    border-bottom: 1px solid lightgray;
+}
+
+#navbar .content-menu
+{
+    display: flex;
+    align-items: center;
+    flex-grow: 1;
+}
+
+#navbar .hamburger
+{
+    display: none;
+}
+
+#navbar h1
+{
+    margin: 0;
+    flex-shrink:0;
+    padding:0px 10px;
+}
+
+#navbar ul
+{
+    margin: 0;
+    list-style-type: none;
+    flex-grow: 1;
+    text-align: right;
+    display: flex;
+    justify-content: flex-end;
+    padding: 0;
+}
+
+#navbar li
+{
+    display: inline-block;
+    padding: 17px 20px;
+    border-left: 1px solid rgb(230,230,230);;
+    min-width: 100px;
+    text-align: center;
+    color:rgb(80,80,80);
+    font-size: 14px;
+}
+
+#navbar li:hover
+{
+  background-color: rgb(240,240,240);
+  color:black;
+}
+
+#navbar a
+{
+    text-decoration: none;
+    color:black;
+}
+
+
+@media only screen and (max-width: 700px)
+{
+    #app
+    {
+        margin-top: 0px !important;
+    }
+
+
+    /**** *** *** *** *** NAVBAR *** *** *** *** ****/
+
+    #navbar
+    {
+        display: block;
+        height: 100vh;
+        color:white;
+        background-color: black;
+        background-image: none;
+        width: 200px;
+        padding:15px;
+    }
+
+    #navbar .content-menu
+    {
+        display: block;
+    }
+
+    #navbar ul
+    {
+        padding: 0;
+        text-align: left;
+    }
+
+    #navbar a
+    {
+        color:white;
+    }
+    #navbar ul
+    {
+      display: block;
+    }
+    #navbar li
+    {
+        display: block;
+        padding: 0;
+        margin: 10px 0px;
+        text-align: left;
+        color:white;
+        border: none;
+    }
+
+    #navbar.noVisible .content-menu
+    {
+        display: none;
+    }
+
+    #navbar .hamburger
+    {
+        filter: invert(100%);
+        height: 50px;
+        width: 50px;    
+        display: block;   
+    }
   
+    #navbar.noVisible
+    {
+        width: 50px;
+        height: 50px;
+    }
+}
+
+
 </style>
