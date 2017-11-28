5cbb3be44a64a80db7807c97aece9b9ac4484b8c
kornakh <larkin@laposte.net>
2017-11-28 17:03:40 +0100

    [ADMIN] added Neons

--- admin/src/components/Admin.vue
+++ admin/src/components/Admin.vue
@@ -1,7 +1,7 @@
 <template lang="html">
   <main>
     <div>
-      <p>plouf</p>
+      <h2 class="neon">Bienvenue dans votre page administrateur </h2>
     </div>
   </main>
 </template>
@@ -17,4 +17,15 @@ export default {
 }
 </script>
 <style lang="css">
+  h2{
+    text-align: center;
+    color : #00F3F9;
+    text-shadow: 0px 0px 5px #00F3F9;
+    font-size: 35px;
+    font:Neons,calibri;
+  }
+  main{
+    background-color: #1f0030;
+  }
+
 </style>
