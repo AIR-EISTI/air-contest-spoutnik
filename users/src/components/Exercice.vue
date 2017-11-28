5fce2881f4d31f9dbe4d5a8e327a927e57ba2959
Gaël Berthaud-Müller <blacksponge@tuta.io>
2017-11-28 21:02:45 +0100

    Load exercice via ajax

--- users/src/components/Exercice.vue
+++ users/src/components/Exercice.vue
@@ -1,9 +1,11 @@
 <template lang="html">
   <main class="exercice center-container">
-    <div class="enonce" v-html="description">
+    <div class="enonce">
+      <h1>{{title}} <span class="date" :title="dateEnd | dateTitle">fin {{dateEnd | date}}</span></h1>
+      <div v-html="description"></div>
     </div>
     <div class="resultat white-container">
-      <h2>Résultats<span :title="dateEnd | dateTitle">fin {{dateEnd | date}}</span></h2>
+      <h2>Résultats</h2>
       <a :href="dataSetUrl">Télécharger le jeu de données.</a>
       <extendable-content-input title="Sortie du programme" ref="output"></extendable-content-input>
       <extendable-content-input title="Code du programme" ref="code"></extendable-content-input>
@@ -16,24 +18,15 @@
 import ExtendableContentInput from '@/components/ExtendableContentInput'
 import * as moment from 'moment'
 import 'moment/locale/fr'
+import * as axios from 'axios'
 export default {
   data () {
     return {
       dataSetUrl: '/api/exercice/1/dataset',
-      dateStart: 1511859303842,
-      dateEnd: 1511859303842,
-      description: `<h1>Super exercice</h1>
-      <h2>What is Lorem Ipsum?</h2>
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-      </p>
-      <h2>Where does it come from?</h2>
-      <p>
-        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-      </p>
-      <p>
-        The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-      </p>`
+      title: '',
+      dateStart: 0,
+      dateEnd: 0,
+      description: ''
     }
   },
   components: {
@@ -41,6 +34,13 @@ export default {
   },
   mounted () {
     moment.locale('fr')
+    axios.get(`/api/exercice/${this.$route.params.id}?markup=html`)
+      .then(response => {
+        this.description = response.data.description
+        this.dateStart = response.data.dateStart
+        this.dateEnd = response.data.dateEnd
+        this.title = response.data.title
+      })
   },
   methods: {
     sendResult () {
@@ -67,20 +67,21 @@ export default {
 }
 </script>
 
-<style lang="css" scoped>
+<style lang="css">
 
 .exercice .resultat h2 {
   margin-top: 0;
 }
 
-.exercice .resultat h2 span {
-  font-size: 0.3em;
+.exercice .enonce span.date {
+  font-weight: normal;
+  font-size: 0.7rem;
   display: inline-block;
   margin-left: 10px;
   padding: 5px;
   background-color: beige;
   position: relative;
-  top: -0.6em
+  top: -0.45rem
 }
 
 .exercice h2, .exercice h1 {
