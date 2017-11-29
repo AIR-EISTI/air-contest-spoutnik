3995cd7acff17c43dbb17c26851b23085e1ada9c
Tjiho Lun <tom.darboux@eisti.fr>
2017-11-29 16:49:48 +0100

    [CSS]:better style for list exercices

--- users/src/components/ListeExercices.vue
+++ users/src/components/ListeExercices.vue
@@ -1,13 +1,33 @@
 <template>
   <main>
-      <input type="search"/>
+      <form id="search">
+        <input type="search"/>
+        <img src="/static/icons/IcoMoon/SVG/135-search.svg"/>
+      </form>
+      
       <section>
             <ul class="tags horizontal">
-                <li>#tournoi</li>
-                <li>#jeu</li>
-                <li>#labyrinthe</li>
-                <li>#snake</li>
-                <li>#graph</li>
+                <li style="border-color:lightblue;">#tournoi</li>
+                <li style="border-color:darkorchid;">#graph</li>
+                <li style="border-color:lightgreen;">#labyrinthe</li>
+                <li style="border-color:orange;">#jeu</li>            
+                
+                <li style="border-color:lightseagreen;">#snake</li>
+                 <li style="border-color:red;">#tournoi</li>
+                <li style="border-color:darkorchid;">#graph</li>
+                <li style="border-color:green;">#labyrinthe</li>
+                <li style="border-color:lightsalmon;">#jeu</li>            
+                
+                <li style="border-color:lightseagreen;">#snake</li>
+                <li style="border-color:green;">#labyrinthe</li>
+                <li style="border-color:lightsalmon;">#jeu</li>            
+                
+                <li style="border-color:lightseagreen;">#snake</li>
+                <li style="border-color:green;">#labyrinthe</li>
+                <li style="border-color:lightsalmon;">#jeu</li>            
+                
+                <li style="border-color:lightseagreen;">#snake</li>
+                
             </ul>
       </section>
       <ul id="listeExercices">
@@ -57,12 +77,74 @@
                 <li class="tag">#graph</li>
               </ul>
           </li>
+          <li class="exercice white-container">
+              <h2 class="title">Super exercice avec un nom très long</h2>
+              <p>
+                  une petite description de l'exercice qui est suivi par un lorem ipsum.Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie. c'est ainsi que l'on peut conclure ce 1er exercice avec un long texte qui nous rpend la tête n'est pas magnifique ?
+              </p>
+              <ul class="tags-fixe">
+                <li class="tag">#snake</li>
+                <li class="tag">#graph</li>
+              </ul>
+          </li>
+          <li class="exercice white-container">
+              <h2 class="title">Super exercice avec un nom très long</h2>
+              <p>
+                  une petite description de l'exercice qui est suivi par un lorem ipsum.Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie. c'est ainsi que l'on peut conclure ce 1er exercice avec un long texte qui nous rpend la tête n'est pas magnifique ?
+              </p>
+              <ul class="tags-fixe">
+                <li class="tag">#snake</li>
+                <li class="tag">#graph</li>
+              </ul>
+          </li>
+          <li class="exercice white-container">
+              <h2 class="title">Super exercice avec un nom très long</h2>
+              <p>
+                  une petite description de l'exercice qui est suivi par un lorem ipsum.Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie. c'est ainsi que l'on peut conclure ce 1er exercice avec un long texte qui nous rpend la tête n'est pas magnifique ?
+              </p>
+              <ul class="tags-fixe">
+                <li class="tag">#snake</li>
+                <li class="tag">#graph</li>
+              </ul>
+          </li>
+          <li class="exercice white-container">
+              <h2 class="title">Super exercice avec un nom très long</h2>
+              <p>
+                  une petite description de l'exercice qui est suivi par un lorem ipsum.Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie. c'est ainsi que l'on peut conclure ce 1er exercice avec un long texte qui nous rpend la tête n'est pas magnifique ?
+              </p>
+              <ul class="tags-fixe">
+                <li class="tag">#snake</li>
+                <li class="tag">#graph</li>
+              </ul>
+          </li>
+          <li class="exercice white-container">
+              <h2 class="title">Super exercice avec un nom très long</h2>
+              <p>
+                  une petite description de l'exercice qui est suivi par un lorem ipsum.Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie. c'est ainsi que l'on peut conclure ce 1er exercice avec un long texte qui nous rpend la tête n'est pas magnifique ?
+              </p>
+              <ul class="tags-fixe">
+                <li class="tag">#snake</li>
+                <li class="tag">#graph</li>
+              </ul>
+          </li>
+          <li class="exercice white-container">
+              <h2 class="title">Super exercice avec un nom très long</h2>
+              <p>
+                  une petite description de l'exercice qui est suivi par un lorem ipsum.Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie. c'est ainsi que l'on peut conclure ce 1er exercice avec un long texte qui nous rpend la tête n'est pas magnifique ?
+              </p>
+              <ul class="tags-fixe">
+                <li class="tag">#snake</li>
+                <li class="tag">#graph</li>
+              </ul>
+          </li>
       </ul>
-      <ul id="pages">
+      <ul id="pages" class="center-container">
+          <li><img src="/static/icons/IcoMoon/SVG/288-backward2.svg"/></li>
           <li>1</li>
           <li>2</li>
           <li>3</li>
           <li>4</li>
+          <li><img src="/static/icons/IcoMoon/SVG/289-forward3.svg"/></li>
       </ul>
 
   </main>
@@ -81,16 +163,38 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-input[type=search]
+
+#search
 {
-    width: 400px;
+    width: 500px;
     display: block;
     margin: auto;
+    position: relative;
+    display: flex;
+    margin-top: 25px;
+    border-bottom:4px solid black;
+    margin-bottom: 15px;
+    max-width: calc(100vw - 40px);
+}
+
+#search input[type=search]
+{
+    width: 100%;
     border:none;
     border-radius: 0px;
-    margin-top: 15px;
-    height: 35px;
-    border: 1px solid lightgrey;
+    margin: 0;
+    height: 38px;
+    background-color: transparent;
+    border-right: none;
+    
+}
+
+#search img
+{
+    height: 28px;
+    width: 28px;
+    padding: 5px;
+    border-left: none;
 }
 
 #listeExercices
@@ -98,19 +202,59 @@ input[type=search]
     list-style-type: none;
     margin: 0;
     padding: 0;
-    max-width: 750px;
+    display: flex;
+    flex-wrap: wrap;
+    max-width: 1200px;
     margin: auto;
-
+    justify-content: center;
 
 }
 
 .exercice
 {
-    margin-top: 15px;
-    border-top: 2px solid navy;
+
+    width: 300px;
+    min-width: 300px;;
+    max-height: 300px;
+    margin: 15px;
+    max-width: 500px;;
+    display: flex;
+    flex-direction: column;
 }
 
+.exercice:nth-child(odd)
+{
+    flex:1;
+}
 
+.exercice:nth-child(even)
+{
+    flex:1;
+}   
+/*
+.exercice:nth-child(4n)
+{
+    
+    flex: 1 !important;
+}
+
+.exercice:nth-child(4n -1)
+{
+    
+    flex: 2 !important;
+}*/
+
+.exercice p
+{
+    overflow: hidden;
+    text-overflow: ellipsis;
+    flex-shrink: 1; 
+}
+
+.exercice .tags-fixe
+{
+    flex-shrink: 0;
+}
 
 
 
@@ -121,7 +265,7 @@ input[type=search]
     max-width: 750px;
     margin: auto;
     padding: 0;
-
+    justify-content: center;
 }
 
 #pages li
@@ -130,6 +274,19 @@ input[type=search]
     border: 1px solid lightgray;
     padding: 5px 10px;
     margin:10px 20px 10px 0px;
+    display: flex;
+
+}
+
+#pages li img
+{
+    width: 20px;
+    height: 20px;
+}
+
+#search_button
+{
+
 }
 
 </style>
