21857c62aaccb3f8329ec765e280363cc85994e8
Tjiho Lun <tom.darboux@eisti.fr>
2018-01-08 20:18:34 +0100

    get list groups from api

--- users/src/components/ListeGroups.vue
+++ users/src/components/ListeGroups.vue
@@ -3,90 +3,38 @@
       <div class="top-container center-big-container">
         <h2>Tournois en cours</h2>
         <div class="grid-1">
-        <div class="text-image tournoi white-container">
-          <article>
-                  <h2 class="center">Tournoi d'hiver</h2>
-                  <div class="infos">
-                      <div class="number-point normal-info">50 points</div>
-                      <div class="date normal-info">depuis 3j</div>
-                  </div>
-                  <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années...</p>
-          </article>
-          <aside>
-              <img src="/static/imgs/ordi2.jpeg"/>
-          </aside>
-        </div>
-        <div class="text-image tournoi white-container">
-          <article>
-                  <h2 class="center">Tournoi d'hiver</h2>
-                  <div class="infos">
-                      <div class="number-point normal-info">50 points</div>
-                      <div class="date normal-info">depuis 3j</div>
-                  </div>
-                  <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 </p>
-          </article>
-          <aside>
-              <img src="/static/imgs/ordi2.jpeg"/>
-          </aside>
-        </div>
+          <router-link :to="{name: 'Groupe',params: { id: group.id }}" class="text-image tournoi white-container" v-for="group in tournaments" :key="group.id">
+            <article>
+                      <h2 class="center">{{group.name}}</h2>
+                      <div class="infos">
+                          <div class="number-point normal-info">{{group.points}} points</div>
+                          <div class="date normal-info">{{group.publicationDate | date}}</div>
+                      </div>
+                      <p>{{group.description}}</p>
+            </article>
+            <aside>
+                <img src="/static/imgs/ordi2.jpeg"/>
+            </aside>
+          </router-link>
         </div>
       </div>
       <div class="center-big-container">
        
         <h2>Les Autres Groupes</h2>
         <div class="grid-2">
-          <div style="background-image:url('/static/imgs/ordi2.jpeg')">
+          <router-link :to="{name: 'Groupe',params: { id: group.id }}" style="background-image:url('/static/imgs/ordi2.jpeg')" v-for="group in groups" :key="group.id">
             <div class="white-container tournoi background">
               
               <article>
-                      <h2 class="center">Tournoi d'hiver</h2>
+                      <h2 class="center">{{group.name}}</h2>
                       <div class="infos">
-                          <div class="number-point normal-info">50 points</div>
-                          <div class="date normal-info">depuis 3j</div>
+                          <div class="number-point normal-info">{{group.points}} points</div>
+                          <div class="date normal-info">{{group.publicationDate | date}}</div>
                       </div>
-                      <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 </p>
+                      <p>{{group.description}}</p>
               </article>
             </div>
-          </div>
-          <div style="background-image:url('/static/imgs/ordi2.jpeg')">
-            <div class="white-container tournoi background">
-              
-              <article>
-                      <h2 class="center">Tournoi d'hiver</h2>
-                      <div class="infos">
-                          <div class="number-point normal-info">50 points</div>
-                          <div class="date normal-info">depuis 3j</div>
-                      </div>
-                      <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 </p>
-              </article>
-            </div>
-          </div>
-          <div style="background-image:url('/static/imgs/ordi2.jpeg')">
-            <div class="white-container tournoi background">
-              
-              <article>
-                      <h2 class="center">Tournoi d'hiver</h2>
-                      <div class="infos">
-                          <div class="number-point normal-info">50 points</div>
-                          <div class="date normal-info">depuis 3j</div>
-                      </div>
-                      <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 </p>
-              </article>
-            </div>
-          </div>
-          <div style="background-image:url('/static/imgs/ordi2.jpeg')">
-            <div class="white-container tournoi background">
-              
-              <article>
-                      <h2 class="center">Tournoi d'hiver</h2>
-                      <div class="infos">
-                          <div class="number-point normal-info">50 points</div>
-                          <div class="date normal-info">depuis 3j</div>
-                      </div>
-                      <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 </p>
-              </article>
-            </div>
-          </div>
+          </router-link>
         </div>
       </div>
 
@@ -95,15 +43,19 @@
 
 <script>
 import * as moment from 'moment'
+import * as axios from 'axios'
 import 'moment/locale/fr'
 export default {
   name: 'ListeGroups',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      groups: [],
+      tournaments: []
     }
   },
   mounted () {
+    this.changePage(0)
   },
   filters: {
     date: function (value) {
@@ -118,14 +70,46 @@ export default {
     }
   },
   methods: {
+    changePage (id) {
+      // this.pageVisible = id
+      // let start = ((id - 1) * this.limit)
+      axios.get('/api/group')
+      .then(response => {
+        this.sortGroups(response.data)
+        scroll(0, 0)
+      })
+    },
+    sortGroups (listGroups) {
+      listGroups.forEach(element => {
+        if (moment().add(1, 'day').isAfter(moment(element.endDate))) {
+          this.groups.push(element)
+        } else {
+          this.tournaments.push(element)
+        }
+      })
+    }
   }
 }
 </script>
 
 <style scoped>
+
+  a
+  {
+      text-decoration: none;
+      color:black;
+  }
+
+  .white-container:hover
+  {
+    border-color: navy !important;
+    
+  }
+
  .tournoi
  {
    margin-bottom: 0px;
+   min-height: 300px;
  }
 
  .grid-2
@@ -154,6 +138,16 @@ export default {
 {
   
   background-color: rgba(255,255,255,0.9);
+}
+
+@media (max-width: 800px) {
+  .grid-2
+  {
+      display: grid;
+      grid-template-columns: repeat(1, 1fr);
+      grid-gap: 15px;
+      /*grid-template-rows: 100px 100px 100px;*/
+  }
 }
 
 </style>
