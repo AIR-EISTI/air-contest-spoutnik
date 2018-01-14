9e00b7226d5769bfc53bd5556d2d2a15a868ea10
Gaël Berthaud-Müller <blacksponge@tuta.io>
2018-01-14 22:36:43 +0100

    Admin - Getting token from localstorage

--- admin/src/utils/UtilsAuth.js
+++ admin/src/utils/UtilsAuth.js
@@ -1,9 +1,10 @@
-var axios = require('axios');
+var axios = require('axios')
 
-var UtilsAuth = {};
-UtilsAuth.logUrl = "/api/token?redirectClientURI=http://localhost:8082/oauth";
-UtilsAuth.token="Bearer mj4h25oj3kdbndvl5t730ks9ret6i8dpli66kt14tngqlniab6";
-UtilsAuth.authRequest = axios.create();
-UtilsAuth.authRequest.defaults.headers.common['Authorization'] = UtilsAuth.token;
+var UtilsAuth = {}
+UtilsAuth.logUrl = "/api/token?redirectClientURI=http://localhost:8082/oauth"
+//UtilsAuth.token="Bearer mj4h25oj3kdbndvl5t730ks9ret6i8dpli66kt14tngqlniab6";
+UtilsAuth.token=`Bearer ${localStorage.getItem('token')}`
+UtilsAuth.authRequest = axios.create()
+UtilsAuth.authRequest.defaults.headers.common['Authorization'] = UtilsAuth.token
 
-module.exports = UtilsAuth;
+module.exports = UtilsAuth
